9711717accf898750d298d04d673cfe151523b04
Boy Setiawan <boye.setiawan@gmail.com>
2019-09-26 10:02:08 +0700

    Initial Commit

---
+++ PBETest2/Helper/DESEncryptor.h
@@ -0,0 +1,43 @@
+//
+//  STEncryptorDES.h
+//
+//
+//  Created by Nimit S. Parekh on 27/01/14.
+//
+//
+
+#import <CommonCrypto/CommonCryptor.h>
+#import <Foundation/Foundation.h>
+
+#define FBENCRYPT_ALGORITHM     kCCAlgorithmDES
+#define FBENCRYPT_BLOCK_SIZE    kCCBlockSizeDES
+#define FBENCRYPT_KEY_SIZE      kCCKeySizeDES
+
+@interface DESEncryptor : NSObject {
+    
+}
+
+//-----------------
+// API (raw data)
+//-----------------
++ (NSData*)generateIv;
++ (NSData*)encryptData:(NSData*)data key:(NSData*)key iv:(NSData*)iv;
++ (NSData*)decryptData:(NSData*)data key:(NSData*)key iv:(NSData*)iv;
+
+
+//-----------------
+// API (base64)
+//-----------------
+// the return value of encrypteMessage: and 'encryptedMessage' are encoded with base64.
+//
++ (NSString*)encryptBase64String:(NSString*)string keyString:(NSString*)keyString separateLines:(BOOL)separateLines;
++ (NSString*)decryptBase64String:(NSString*)encryptedBase64String keyString:(NSString*)keyString;
+
+//-----------------
+// API (utilities)
+//-----------------
++ (NSString*)hexStringForData:(NSData*)data;
++ (NSData*)dataForHexString:(NSString*)hexString;
+
+
+@end
---
+++ PBETest2/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PBETest2
+//
+//  Created by boy setiawan on 26/09/19.
+//  Copyright © 2019 boy setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let plain = "PlainText"
+        let plainData = plain.data(using: String.Encoding.ascii)
+        let key = "SecretKey"
+        let keyData = key.data(using: String.Encoding.ascii)
+        
+        let iv = [0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef]
+        let ivData =  NSKeyedArchiver.archivedData(withRootObject: iv)
+        
+        
+        let encryptedData = DESEncryptor.encryptData(plainData, key: keyData, iv: ivData)
+        print("\(encryptedData)")
+        
+        let str = String(decoding: encryptedData!, as: UTF8.self)
+        print("\(str)")
+        
+        let decryptedData = DESEncryptor.decryptData(encryptedData, key: keyData, iv: ivData)
+        print("\(decryptedData)")
+        let strOrigional = String(decoding: decryptedData!, as: UTF8.self)
+        print("\(strOrigional)")
+    }
+
+
+}
+
